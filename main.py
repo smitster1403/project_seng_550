f6959943c67dc850e9fd19002881f52ba9574ff6
ktfr <112982272+ktfr@users.noreply.github.com>
2024-11-27 16:48:34 -0700

    Add py file to test Spark database creation

---
+++ main.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import pyspark
+from collections import Counter
+import matplotlib.pyplot as plt
+
+from pyspark.sql import SparkSession
+
+
+data = pd.read_csv('the-reddit-climate-change-dataset-comments.csv')
+data = data.dropna() # drop any rows with missing values
+
+# counter = Counter(data['subreddit.nsfw'])
+# print(counter)
+
+# print(data['score'])
+#print(data['body'].iloc[20340])
+
+
+# initialize spark session
+spark = SparkSession.builder \
+    .appName("Reddit Climate Change Comments") \
+    .config("spark.driver.memory", "4g") \
+    .config("spark.executor.memory", "8g") \
+    .config("spark.sql.shuffle.partitions", "100") \
+    .getOrCreate()
+
+df = spark.read.csv("the-reddit-climate-change-dataset-comments.csv", header=True, inferSchema=True)
+df = df.repartition(100)  # Increase the number of partitions for large datasets
+df.show()
+
+#create spark db
+spark.sql("CREATE DATABASE reddit_db")
+
+#create table with comments
+spark.sql("USE reddit_db")
+df.write.mode("overwrite").saveAsTable("reddit_db.comments")
+
+spark.sql("SHOW TABLES").show()
+
+col_names = df.columns
+print(col_names)
+
+filtered_db = spark.sql("SELECT id, body, score FROM comments LIMIT 5")
+filtered_db.show()
+
+print(filtered_db)
