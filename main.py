6b7cbf118f2b34a6c1d0467a3e7c6e27951294f0
ktfr <112982272+ktfr@users.noreply.github.com>
2024-12-17 20:35:08 -0700

    add validation

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
-import pyspark
-from collections import Counter
+# import pyspark
+# from collections import Counters
 import matplotlib.pyplot as plt
 
 from pyspark.sql import SparkSession
 
 
-data = pd.read_csv('the-reddit-climate-change-dataset-comments.csv')
-data = data.dropna() # drop any rows with missing values
+#data = pd.read_csv('the-reddit-climate-change-dataset-comments.csv')
+#data = data.dropna() # drop any rows with missing values
 
 # counter = Counter(data['subreddit.nsfw'])
 # print(counter)
@@ -26,8 +26,14 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 df = spark.read.csv("the-reddit-climate-change-dataset-comments.csv", header=True, inferSchema=True)
-df = df.repartition(100)  # Increase the number of partitions for large datasets
+df = df.repartition(100)  # increase the number of partitions for large datasets
 df.show()
+
+df_original = df # save original dataset
+
+# -- clean dataset ---
+#remove null values first
+df = df.dropna()
 
 #create spark db
 spark.sql("CREATE DATABASE reddit_db")
